9247c1e1d1d0d76794b50716a5b742a29cce7424
Justin <justintt.lee@gmail.com>
2024-03-10 17:38:42 -0400

    big push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      cover: z
+        .object({
+          image: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      tags: z.array(z.string()),
+    }),
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
